a2cbc765c5e835f0060cbb1d51406e4096ce560a
gavand-sagar <gavand.sagar.s@gmail.com>
2022-07-24 23:21:00 +0530

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from 'cors';
 import userRoutes from './routes/users.routes.js'
 import mathRoutes from './routes/math.routes.js'
 import utilsRoutes from "./routes/test-util-routes.js";
 import postRoutes from './routes/posts.routes.js'
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 app.use(userRoutes);
 app.use(mathRoutes);
